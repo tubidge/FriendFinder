1e203643beb2285df452cc293d10e21be965de25
tubidge <tubidge@gmail.com>
2019-05-06 22:35:25 -0600

    partly working submission

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,58 +1,58 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'R0otsNh0ots9',
-    database: 'friends_db'
-});
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'R0otsNh0ots9',
+//     database: 'friends_db'
+// });
 
-connection.connect(function (err) {
-    if (err) throw err;
+// connection.connect(function (err) {
+//     if (err) throw err;
 
-});
+// });
 
-function Friend(name, photo, scores) {
-    this.name = name;
-    this.photo = photo;
-    this.scores = scores
-};
+// function Friend(name, photo, scores) {
+//     this.name = name;
+//     this.photo = photo;
+//     this.scores = scores
+// };
 
-Friend.prototype.getArr = function () {
-    var scores = this.scores.split(", ");
-    // console.log(scores);
-}
+// Friend.prototype.getArr = function () {
+//     var scores = this.scores.split(", ");
+// }
 
-var friends = [];
 
-connection.query("SELECT * FROM friends", function (err, res) {
-    if (err) throw err;
-    var name;
-    var photo;
-    var scores;
-    for (var i = 0; i < res.length; i++) {
-        name = res[0].name;
-        photo = res[0].photo;
-        scores = res[0].scores;
-    };
+// connection.query("SELECT * FROM friends", function (err, res) {
+//     if (err) throw err;
+//     var name;
+//     var photo;
+//     var scores;
+//     for (var i = 0; i < res.length; i++) {
+//         name = res[i].name;
+//         photo = res[i].photo;
+//         scores = res[i].scores;
+//     };
 
-    var friend = new Friend(name, photo, scores);
-    friend.getArr();
+//     var friend = new Friend(name, photo, scores);
+//     friend.getArr();
 
-    friends.push(friend);
-});
+//     for (var i = 0; i < res.length; i++) {
+//         friends.push(friend);
+//     };
+// });
 
-// var friends = [{
-//         name: "Randy",
-//         photo: "https://vignette.wikia.nocookie.net/spsot/images/b/b9/Randy_Marsh_facebook_profile.png/revision/latest?cb=20141024175401",
-//         scores: [5, 4, 2, 5, 1, 3, 5, 2, 4, 1]
-//     },
-//     {
-//         name: "Gerald",
-//         photo: "https://vignette.wikia.nocookie.net/spsot/images/e/e2/Gerald_Broflovski_facebook_profile.png/revision/latest?cb=20141118173207",
-//         scores: [4, 3, 1, 3, 4, 5, 3, 4, 3, 2]
-//     }
-// ];
+var friends = [{
+        name: "Randy",
+        photo: "https://vignette.wikia.nocookie.net/spsot/images/b/b9/Randy_Marsh_facebook_profile.png/revision/latest?cb=20141024175401",
+        scores: [5, 4, 2, 5, 1, 3, 5, 2, 4, 1]
+    },
+    {
+        name: "Gerald",
+        photo: "https://vignette.wikia.nocookie.net/spsot/images/e/e2/Gerald_Broflovski_facebook_profile.png/revision/latest?cb=20141118173207",
+        scores: [4, 3, 1, 3, 4, 5, 3, 4, 3, 2]
+    }
+];
 
-// module.exports = friends;
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,15 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        console.log(req.body.scores);
+        // move this totalScore function to survey.html
+        var totalScore = 0;
+        for (var i = 0; i < req.body.scores.length; i++) {
+            totalScore += parseInt(req.body.scores[i]);
+        };
+        console.log(totalScore);
         friends.push(req.body);
+        // create logic to compare new user to prior users.
+
     });
 };

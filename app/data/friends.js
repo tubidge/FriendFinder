0d6fc2aa462510df4b476c6af05bcd5e435c49eb
tubidge <tubidge@gmail.com>
2019-05-08 20:35:47 -0600

    adding comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -46,12 +46,26 @@
 var friends = [{
         name: "Randy",
         photo: "https://vignette.wikia.nocookie.net/spsot/images/b/b9/Randy_Marsh_facebook_profile.png/revision/latest?cb=20141024175401",
-        scores: [5, 4, 2, 5, 1, 3, 5, 2, 4, 1]
+        scores: [5, 4, 2, 5, 1, 3, 5, 2, 4, 1],
+        totalScore: 32
     },
     {
         name: "Gerald",
         photo: "https://vignette.wikia.nocookie.net/spsot/images/e/e2/Gerald_Broflovski_facebook_profile.png/revision/latest?cb=20141118173207",
-        scores: [4, 3, 1, 3, 4, 5, 3, 4, 3, 2]
+        scores: [4, 3, 1, 3, 4, 5, 1, 4, 3, 2],
+        totalScore: 30
+    },
+    {
+        name: "Sheila",
+        photo: "https://vignette.wikia.nocookie.net/spsot/images/7/7d/Sheila_friend_icon.png/revision/latest?cb=20140425143321",
+        scores: [4, 3, 1, 3, 4, 5, 1, 4, 1, 2],
+        totalScore: 28
+    },
+    {
+        name: "Sharon",
+        photo: "https://vignette.wikia.nocookie.net/spsot/images/6/6d/Sharon_Marsh_facebook_profile.png/revision/latest?cb=20141118210612",
+        scores: [4, 3, 5, 3, 4, 5, 5, 4, 1, 2],
+        totalScore: 36
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,15 +6,39 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body.scores);
-        // move this totalScore function to survey.html
-        var totalScore = 0;
+        // variable to hold the current user's total score, used for comparison with next function.
+        var newTotalScore = 0;
+        // array to hold each total score difference between existing friends and new user.
+        // then we can determine which difference is the smallest,
+        // and use that number's index to find it's user.
+        var scoreDiffs = [];
+
+        // creating the new user's total score for comparison use.
         for (var i = 0; i < req.body.scores.length; i++) {
-            totalScore += parseInt(req.body.scores[i]);
+            newTotalScore += parseInt(req.body.scores[i]);
         };
-        console.log(totalScore);
+        req.body.totalScore = newTotalScore;
+        console.log(req.body);
+        // pushing the new user object to the friends data array.
         friends.push(req.body);
-        // create logic to compare new user to prior users.
+
+        // for loop to create score differences for each user comparison
+        for (var i = 0; i < friends.length; i++) {
+            console.log(`comparing friend: ${friends[i].name}`);
+            var currComparison = friends[i].totalScore;
+            if (newTotalScore > currComparison) {
+                scoreDiffs.push(newTotalScore - currComparison);
+            } else if (newTotalScore < currComparison) {
+                scoreDiffs.push(currComparison - newTotalScore);
+            }
+            // else if (newTotalScore === currComparison) {
+            //     console.log(`Friend ${friends[i].name} has the same score!`);
+            // };
+        };
+
+        console.log(`Score Differences: ${scoreDiffs}`);
+        // now determine which score diff is the lowest, find it's index, and return that friend.
 
     });
+
 };
